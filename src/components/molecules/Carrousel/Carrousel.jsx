eb93ba82fc5a8003f0e8cb765de970f290f98b47
emelinew1 <emeline.wulstecke@gmail.com>
2024-05-07 11:31:20 +0200

    Modal for carrousel created

--- src/components/molecules/Carrousel/Carrousel.jsx
+++ src/components/molecules/Carrousel/Carrousel.jsx
@@ -2,29 +2,49 @@ import React, { useState } from 'react';
 import './carrousel.css';
 import { portfolio } from '../../../assets/data.json';
 import Card from '../Card/Card';
-import Modal from '../Modal/Modal'; // Assurez-vous d'importer correctement le composant Modal
+import Modal from '../Modal/Modal';
+import Link from '../../atoms/link/Link'; 
+import Button from '../../atoms/Button/Button';
 
 const Carrousel = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedProjectId, setSelectedProjectId] = useState(null);
 
-    const openModal = (project) => {
-        setSelectedProject(project);
+    const openModal = (projectId) => {
+        setSelectedProjectId(projectId);
         setIsModalOpen(true);
     };
 
     const closeModal = () => {
         setIsModalOpen(false);
-        setSelectedProject(null);
+        setSelectedProjectId(null);
     };
+
+    // Fonction pour récupérer les détails du projet à partir de son identifiant
+    const getProjectById = (projectId) => {
+        return portfolio.find(project => project.id === projectId);
+    };
+
+    const selectedProject = selectedProjectId ? getProjectById(selectedProjectId) : null;
 
     return (
         <div className="portfolio">
-            {portfolio.map((project, index) => (
-                <Card key={index} project={project} onCardClick={() => openModal(project)} />
+            {portfolio.map((project) => (
+                <Card key={project.id} project={project} onCardClick={() => openModal(project.id)} />
             ))}
             <Modal isOpen={isModalOpen} onClose={closeModal}>
-                {/* Contenu de votre modal, utilisez selectedProject pour afficher les informations */}
+                {selectedProject && (
+                    <article className='modal-carrousel'>
+                        <h3>{selectedProject.name}</h3>
+                        <p>{selectedProject.texte}</p>
+                        <div>
+                        <Link url={selectedProject.url} content={<Button text="Site" />} />
+                        {selectedProject.github && (
+                            <Link url={selectedProject.github} content={<Button text="Code" />} />
+                        )}
+                        </div>
+                    </article>
+                )}
             </Modal>
         </div>
     );
